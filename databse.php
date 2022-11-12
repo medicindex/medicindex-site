73a1564b5fe2189fe0c165a58ea18a917e043959
Rudransh Arora <rudransh1482@gmail.com>
2022-11-12 10:48:40 +0530

    Deployed database

--- databse.php
+++ databse.php
@@ -1,42 +1,9 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <title>Database</title>
-</head>
-<body>
-    <style>
-        body{
-            background-image: url('medbg.jpg');
-        }
-        .headingg{
-            
-            text-align: center;
-            font-size: 54px;
-            color: rgb(255, 255, 255);
-            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-            text-shadow: 5px 5px 8px #000000;
-        }
-        .head{
-            display: inline-block;
-        }
-    </style>
-    
-    <input type="button" class="btn btn-primary ml-4" value="Go back!" onclick="history.back()">
-    <p class="headingg m-0">medicIndex</p>
-    
-    <iframe class="airtable-embed" src="https://airtable.com/embed/shryoBuMUSSyQlckS?backgroundColor=yellow&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
-</body>
-</html> -->
+
 
 <?php
 
 //Step1
- $db = mysqli_connect('localhost','root','','medicindex')
+ $db = mysqli_connect('sql110.epizy.com','epiz_32980507','Fs5b4qusZH','epiz_32980507_meddata')
  or die('Error connecting to MySQL server.');
 ?>
 
